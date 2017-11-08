1bb878bb0f93bd4f63a63477d0295fa109db5935
Suo Qin <kpsesuo@gmail.com>
2017-11-08 19:31:54 +1100

    add tests dependencies

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,29 @@
 <template>
-    <div>
+    <div id="app">
+        <MessageList :messages="messages"/>
     </div>
 </template>
 <script>
+    import MessageList from '../components/MessageList'
     export default {
         created () {
-            this.$model.home()
-                .then(data => {
-                    console.log("data: ", data);
-                });
-        }
+//            this.$model.home()
+//                .then(data => {
+//                    console.log("data: ", data);
+//                });
+        },
+      data () {
+          return {
+            messages: ['Hey John', 'Howdy Paco']
+          }
+      },
+      components: {
+        MessageList
+      }
     }
 </script>
 <style lang="less" scoped>
-	
+    div {
+        font-size: 14pt;
+    }
 </style>
