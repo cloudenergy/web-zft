e5a78ac0ed287a1ba8129476c877c2e73c51f1e5
Suo Qin <kpsesuo@gmail.com>
2017-11-11 00:14:51 +1100

    build basic bill page structure

---
+++ modules/bill/index.js
@@ -0,0 +1,4 @@
+export { default as BillStatus } from "./bill-status";
+export { default as PaymentStatus } from "./payment-status";
+export { default as ReportPeriod } from "./report-period";
+export { default as DataTable } from "./data-table";
