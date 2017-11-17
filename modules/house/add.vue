7c148fb5ee6a5bd68e271e9940ff040e30319f39
insane <luojie@medlinker.com>
2017-11-17 09:44:41 +0800

    添加checkbox

--- modules/house/add.vue
+++ modules/house/add.vue
@@ -1,6 +1,7 @@
 <template>
     <el-dialog title="房源信息" :visible="true">
         <el-form :model="form" class="mini-form">
+            <h3>房源信息</h3>
             <div class="group">
                 <el-form-item label="方式">
                     <el-input v-model="form.name" auto-complete="off"></el-input>
@@ -31,6 +32,19 @@
                     <el-input v-model="form.name" auto-complete="off"></el-input>
                 </el-form-item>
             </div>
+            <h3>房源配置</h3>
+            <div class="config checkboxes">
+                <el-checkbox-group v-model="form.type">
+                    <el-checkbox label="全选" name="type"></el-checkbox>
+                    <el-checkbox label="WIFI" name="type"></el-checkbox>
+                    <el-checkbox label="油烟机" name="type"></el-checkbox>
+                    <el-checkbox label="停车位" name="type"></el-checkbox>
+                    <el-checkbox label="空调" name="type"></el-checkbox>
+                    <el-checkbox label="阳台" name="type"></el-checkbox>
+                    <el-checkbox label="煤气" name="type"></el-checkbox>
+                    <el-checkbox label="游泳池" name="type"></el-checkbox>
+                </el-checkbox-group>
+            </div>
         </el-form>
         <div slot="footer" class="dialog-footer">
             <el-button @click="close()">取 消</el-button>
@@ -56,6 +70,27 @@
 <style lang="less" scoped>
     .dialog-footer {
     	text-align: right;
+    }
+
+    .el-form {
+    	h3 {
+    		margin-bottom: 20px;
+    	}
+
+    	.el-checkbox-group {
+    		width: 100%;
+    	}
+
+    	.checkboxes {
+    		display: flex;
+    		flex-wrap: wrap;
+    		justify-content: space-between;
+
+    		.el-checkbox {
+    			width: 20%;
+    			margin-left: 0px;
+    		}
+    	}
     }
 </style>
 
