548f391ef6d09f149ca11971fcde9690310d69f2
Suo Qin <kpsesuo@gmail.com>
2017-11-18 09:39:08 +1100

    refine id input

--- modules/contract/add-contract.vue
+++ modules/contract/add-contract.vue
@@ -27,9 +27,20 @@
 								<template slot="prepend">性别</template>
 							</el-input>
 						</el-col>
-						<el-col :span="16">
-							<el-input placeholder="选填" v-model="form.profile.idNumber">
-								<template slot="prepend">身份证</template>
+						<el-col :span="5">
+							<el-select v-model="form.profile.idType" class="id-type">
+								<el-option label="身份证" value="1"></el-option>
+								<el-option label="护照" value="2"></el-option>
+								<el-option label="港澳通行证" value="3"></el-option>
+								<el-option label="台胞证" value="4"></el-option>
+								<el-option label="居住证" value="5"></el-option>
+								<el-option label="临时居住证" value="6"></el-option>
+								<el-option label="营业执照" value="7"></el-option>
+								<el-option label="其他证件" value="8"></el-option>
+							</el-select>
+						</el-col>
+						<el-col :span="11">
+							<el-input placeholder="证件号(选填)" v-model="form.profile.idNumber">
 							</el-input>
 						</el-col>
 					</el-row>
@@ -168,8 +179,10 @@
 
 <script>
 	import Modal from '~/plugins/modal';
+	import ElCol from "element-ui/packages/col/src/col";
 
 	export default {
+		components: {ElCol},
 		data() {
 			return {
 				form: {
@@ -178,7 +191,8 @@
 						account: '',
 						phone: '',
 						gender: '',
-						idNumber: ''
+						idNumber: '',
+						idType: '1'
 					},
 					property: {
 						houseType: '1',
