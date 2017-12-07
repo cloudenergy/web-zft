d01f50d9af70c78fae54082444b6008cd44f2915
Suo Qin <kpsesuo@gmail.com>
2017-12-07 20:01:26 +1100

    change docker registry

--- docker/script/deploy.sh
+++ docker/script/deploy.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
 IMAGE_VERSION=${1:-latest}
-docker pull registry.docker-cn.com/kpse/web-zft:$IMAGE_VERSION
+REPO=registry.cn-hangzhou.aliyuncs.com/em_test/web-zft
+docker pull $REPO:$IMAGE_VERSION
 
 function start () {
   docker rm -f web
-  docker rmi $(docker images -qf "before=registry.docker-cn.com/kpse/web-zft:$IMAGE_VERSION" -f=reference='registry.docker-cn.com/kpse/web-zft:v*')
-  docker run -d -e ZFT_BACKEND_PROXY -p 80:80 --net=zft --name=web registry.docker-cn.com/kpse/web-zft:$IMAGE_VERSION
+  docker rmi $(docker images -qf "before=$REPO:$IMAGE_VERSION" -f=reference="$REPO:v*")
+  docker run -d -e ZFT_BACKEND_PROXY -p 80:80 --net=zft --name=web $REPO:$IMAGE_VERSION
 }
 function test_deploy() {
-  docker rm -f test_deploy  
-  docker run -d --name test_deploy -p 8080:80 --net=zft registry.docker-cn.com/kpse/web-zft:$IMAGE_VERSION
+  docker rm -f test_deploy
+  docker run -d --name test_deploy -p 8080:80 --net=zft $REPO:$IMAGE_VERSION
 }
 function test_curl() {
   response=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080)
@@ -25,5 +26,4 @@ test_deploy
 
 sleep 3
 
-test_curl
-start
+test_curl && start
