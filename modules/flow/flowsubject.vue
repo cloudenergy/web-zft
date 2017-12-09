01faa479ebb141bd64cfca9609d598ba4ea3cf07
ZSH-HSZ <296727088@qq.com>
2017-12-09 23:07:39 +0800

    警告消除，代码修改

--- modules/flow/flowsubject.vue
+++ modules/flow/flowsubject.vue
@@ -3,31 +3,8 @@
     <div class="block" style="margin-bottom:10px">
         <el-form :inline="true" :model="formInline" class="demo-form-inline  flexc flexce" size="mini">   
           <goend/>
-          <span class="marsp marspa ">承租方式</span>
-          <el-form-item label="" style="margin:0">
-            <el-select v-model="formInline.type" placeholder="承租方式" class="minisel">
-              <el-option label="全部" value="1"></el-option>
-              <el-option label="合租" value="2"></el-option>
-              <el-option label="整租" value="3"></el-option>
-              <el-option label="整栋" value="4"></el-option>
-            </el-select>
-          </el-form-item>
-          <span class="marsp marspa ">城市</span>
-          <el-form-item label="" style="margin:0">
-            <el-select v-model="formInline.city" placeholder="城市" class="minisel">
-              <el-option label="杭州" value="1"></el-option>
-              <el-option label="背景" value="2"></el-option>
-              <el-option label="上海" value="3"></el-option>
-            </el-select>
-          </el-form-item>
-          <span class="marsp marspa ">小区</span>
-          <el-form-item label="" style="margin:0">
-            <el-select v-model="formInline.community" placeholder="小区" class="minisel">
-              <el-option label="1号小区" value="1"></el-option>
-              <el-option label="2号小区" value="2"></el-option>
-              <el-option label="3号小区" value="3"></el-option>
-            </el-select>
-          </el-form-item>
+          <tenant-way class="marsp marspa"/>
+          <city-area style="width:220px" class="flexce"/>
           <div class="importres">
             <div class="actions">
               <el-button type="warning" size="mini">
@@ -45,11 +22,14 @@
 <script>
 import flowsubjectlist from './flowsubjectlist.vue'
 import goend from './goend.vue'
-
+import mounth from './mounth.vue'
+import ofpayments from './ofpayments.vue'
 export default {
   components: {
       flowsubjectlist,
-      goend
+      goend,
+      ofpayments,
+      mounth
   },
   data() {
     return {
---
+++ modules/flow/mounth.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline  flexc flexce" size="mini">
+        <el-form-item label="" style="margin:0">
+            <el-select v-model="formInline.type" placeholder="年份" class="minisel" @change="changeflow">
+                <el-option :label="item.label" v-for="item in formInlinetype" :key="item.value" :value="item.value"></el-option>
+            </el-select>
+        </el-form-item>
+    </el-form>
+</template>
+<script>
+export default {
+    data() {
+        return{
+            formInline:{
+                type:''
+            },
+            formInlinetype:[
+                {
+                    value:1,
+                    label:'1990'
+                },
+                {
+                    value:2,
+                    label:'1991'
+                },
+                {
+                    value:3,
+                    label:'1992'
+                }
+            ]
+        }
+    },
+    methods:{
+        changeflow(){
+            console.log(this.formInline.type)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.flexce{
+    align-items: center;
+  }
+  .minisel{
+    width: 100px;
+  }
+</style>
+
