ccbe269c99a24843114e6bfe640187d1ebc5e496
sjj0222 <429832306@qq.com>
2017-12-12 18:15:45 +0800

    账单优化

--- modules/bill/data-table.vue
+++ modules/bill/data-table.vue
@@ -158,7 +158,6 @@
                     </template>
                 </el-table-column>
 
-<<<<<<< HEAD
     <el-table-column property="bill_type" label="帐单22类型"></el-table-column>
     <el-table-column property="bill_cycle" label="费用周期"></el-table-column>
     <el-table-column property="bill_unit" label="单价"></el-table-column>
@@ -189,14 +188,6 @@
     <el-button type="primary" @click="dialog_edit = false">确 定</el-button>
   </span>
 </el-dialog>
-=======
-            </el-table>
-            <span slot="footer" class="dialog-footer">
-                <el-button @click="dialog_edit = false">取 消</el-button>
-                <el-button type="primary" @click="dialog_edit = false">确 定</el-button>
-            </span>
-        </el-dialog>
->>>>>>> 7b15c5dc946567cc87cfaca24b29b8bff5d3d900
 
         <!-- 备注 -->
         <el-dialog :title='dialogTitle' :visible.sync="dialog_remark" width='30%'>
@@ -228,7 +219,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 export default {
 	data() {
 		return {
@@ -404,190 +394,6 @@ export default {
 		}
 	}
 };
-=======
-    export default {
-    	data() {
-    		return {
-    			dialogVisible: false,
-    			dialogTitle: 'Tips',
-    			// dialog_collection: true,
-    			tableData: [
-    				{
-    					overdue: '逾期63天',
-    					date: '2016-05-03',
-    					name: '郭德纲',
-    					rent: 1200,
-    					phone: '1300000001',
-    					rentDesc: '租金6期',
-    					address: '朝晖三区·2栋2单元201室·B2017-07-01 → 2017-07-31'
-    				},
-    				{
-    					overdue: '逾期62天',
-    					date: '2016-05-02',
-    					name: '老刘',
-    					rent: 122,
-    					rentDesc: '押金',
-    					phone: '1300000002',
-    					address: '龙湖滟澜山·8栋2单元203室·G2017-08-01 → 2017-08-20'
-    				},
-    				{
-    					overdue: '逾期61天',
-    					date: '2016-05-04',
-    					name: '冰儿',
-    					rent: 2200,
-    					rentDesc: '租金11期',
-    					phone: '1300000003',
-    					address: '朝晖三区·3栋3单元301室·改哎的名字2017-08-22 → 2017-11-21'
-    				},
-    				{
-    					overdue: '',
-    					date: '2016-05-01',
-    					name: '不是整天 ',
-    					rent: 200,
-    					rentDesc: '押金',
-    					phone: '1300000004',
-    					address:
-    						'保利香槟国际·2栋2单元2室·这个名字很长啊这个字啊这个名字很长啊这个名字很长啊这个名字很长啊2017-10-12 → 2018-04-11'
-    				}
-    			],
-    			table_collection: [
-    				{
-    					index: 0,
-    					bill_type: '常规押金',
-    					bill_cycle: '2017-07-01 至 2017-12-31',
-    					bill_unit: '-',
-    					bill_receivable: '1000',
-    					bill_dueIn: '1000',
-    					bill_ealPrice: '1000',
-    					bill_balance: '暂无余额'
-    				},
-    				{
-    					index: 1,
-    					bill_type: '常规押金',
-    					bill_cycle: '2017-07-01 至 2017-12-31',
-    					bill_unit: '-',
-    					bill_receivable: '1100',
-    					bill_dueIn: '1100',
-    					bill_ealPrice: '1100',
-    					bill_balance: '暂无余额'
-    				}
-    			],
-    			dialog_collection: false,
-    			dialog_edit: false,
-    			dialog_remark: false,
-    			dialog_take_a_rain_check: false,
-    			// 余额
-    			bill_balance: '',
-    			// 待收
-    			bill_dueIn: '',
-    			// 实收
-    			bill_ealPrice: '',
-    			// 改期
-    			billData: '',
-    			// 备注
-    			billTextarea: '',
-    			table_collection2: [
-    				{
-    					table_collection2_data: '2017-12-09 14:54:21',
-    					table_collection2_pay: [
-    						'现金',
-    						'转账',
-    						'微信',
-    						'支付宝',
-    						'刷卡',
-    						'其他'
-    					],
-    					table_collection2_name: ['x', 'h']
-    				}
-    			],
-    			table_collection2_name_val: '',
-    			table_collection2_pay_val: '',
-    			input3: '',
-    			dialog_edit_v: '',
-    			edit_msg: ''
-    		};
-    	},
-    	methods: {
-    		/**@augments
-    		 * max 传入实收最大的值
-    		 * num 实收的值
-    		 */
-
-    		ealOrice(max, num) {
-    			if (this.bill_ealPrice > max) {
-    				this.bill_ealPrice = max;
-    			}
-    			console.log(this.bill_ealPrice);
-    			console.log(max);
-    			return (this.bill_ealPrice = num);
-    		},
-    		handleEdit(index, row) {
-    			this.dialog_edit = true;
-    			this.dialogTitle = '编辑账单';
-    			console.log(index, row);
-    		},
-    		handleReceive(index, row) {
-    			this.dialog_collection = true;
-    			this.dialogTitle = '处理账单';
-    			console.log(index, row);
-    		},
-    		addComment(index, row) {
-    			this.dialog_remark = true;
-    			this.dialogTitle = '添加备注';
-    			console.log(index, row);
-    		},
-    		changeDate(index, row) {
-    			this.dialog_take_a_rain_check = true;
-    			this.dialogTitle = '修改应支付日';
-    			console.log(index, row);
-    		},
-    		handleClose(done) {
-    			this.$confirm('Are you sure to close this dialog?')
-    				.then(() => {
-    					done();
-    				})
-    				.catch(() => {});
-    		},
-    		editInput(rowNub) {
-    			let edit_v = parseFloat(this.dialog_edit_v);
-    			let row_nub = parseFloat(rowNub);
-    			if (edit_v > row_nub) {
-    				this.edit_msg = '加收' + (edit_v - row_nub);
-    			} else if (edit_v < row_nub) {
-    				this.edit_msg = '减免' + (row_nub - edit_v);
-    			} else {
-    				this.edit_msg = '';
-    			}
-    			// console.log(i)
-    		},
-    		edit_remove(index) {
-    			this.table_collection.splice(index, 1);
-    		},
-    		openMsg(index) {
-    			this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
-    				confirmButtonText: '确定',
-    				cancelButtonText: '取消',
-    				type: 'warning'
-    			})
-    				.then(() => {
-    					this.edit_remove();
-    					this.$message({
-    						type: 'success',
-    						message: '删除成功!'
-    					});
-    				})
-    				.catch(() => {
-    					this.$message({
-    						type: 'info',
-    						message: '已取消删除'
-    					});
-    				});
-
-    			// console.log(this.$message)
-    		}
-    	}
-    };
->>>>>>> 7b15c5dc946567cc87cfaca24b29b8bff5d3d900
 </script>
 <style>
     .table_collection2 {
