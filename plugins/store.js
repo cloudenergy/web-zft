a7b0baa1b94cf4e3b595c85582b6eb34181824d4
jie <luojie@doctorwork.com>
2018-01-17 10:38:18 +0800

    修改获取小区实现

--- plugins/store.js
+++ plugins/store.js
@@ -10,65 +10,75 @@ import { fromPairs, merge, map } from 'lodash';
 
 let localUser = '';
 try {
-	localUser = JSON.parse(localStorage.user);
+    localUser = JSON.parse(localStorage.user);
 } catch (e) {
-	localUser = {
-		projectId: 100
-	};
+    localUser = {
+        projectId: 100
+    };
 }
 
 export default {
-	state: {
-		user: {
-			...localUser
-		},
-		cityArea: {},
-		houseTypes: {
-			SHARE: ['SHARE', '合租'],
-			SOLE: ['SOLE', '整租'],
-			ENTIRE: ['ENTIRE', '整栋']
-		},
-		defaultHouseType: 'SOLE'
-	},
-	mutations: {
-		UPDATE_ENV(state, data) {
-			Object.keys(data).forEach(item => {
-				state[item] = data[item];
-			});
-		}
-	},
-	actions: {
-		POST_LOGIN({ commit, state }) {
-			return api('login', {
-				username: 'admin100',
-				password: '5f4dcc3b5aa765d61d8327deb882cf99'
-			});
-		},
+    state: {
+        user: {
+            ...localUser
+        },
+        cityArea: {},
+        houseTypes: {
+            SHARE: ['SHARE', '合租'],
+            SOLE: ['SOLE', '整租'],
+            ENTIRE: ['ENTIRE', '整栋']
+        },
+        defaultHouseType: 'SOLE',
+        communities: null
+    },
+    mutations: {
+        UPDATE_ENV(state, data) {
+            Object.keys(data).forEach(item => {
+                state[item] = data[item];
+            });
+        },
+        SAVE_COMMUNITIES(state, data) {
+            state.communities = data;
+        }
+    },
+    actions: {
+        POST_LOGIN({ commit, state }) {
+            return api('login', {
+                username: 'admin100',
+                password: '5f4dcc3b5aa765d61d8327deb882cf99'
+            });
+        },
 
-		GET_ENVIRONMENTS({ commit, state }) {
-			return api('environments')
-				.then(env => fromPairs(map(env, i => [i.key, i.value])))
-				.then(env => {
-					commit('UPDATE_ENV', env);
+        GET_ENVIRONMENTS({ commit, state }) {
+            return api('environments')
+                .then(env => fromPairs(map(env, i => [i.key, i.value])))
+                .then(env => {
+                    commit('UPDATE_ENV', env);
 
-					// sync user
-					localStorage.user = JSON.stringify({
-						auth: true,
-						...env.user
-					});
-				});
-		},
-		GET_COMMUNITIES({ commit, state }, { houseType }) {
-			return api('communities').query(
-				{ houseFormat: houseType },
-				{ projectId: state.user.projectId }
-			);
-		},
-		GET_CITY_AREA({ commit, state }) {
-			if (Object.keys(state.city_area).length) {
-				return state.city_area;
-			}
-			return api('city_area');
-		}
-	}
+                    // sync user
+                    localStorage.user = JSON.stringify({
+                        auth: true,
+                        ...env.user
+                    });
+                });
+        },
+        GET_COMMUNITIES({ commit, state }, { houseType }) {
+            if (state.communities) {
+                return Promise.resolve(state.communities);
+            }
+            return api('communities').query(
+                { houseFormat: houseType },
+                { projectId: state.user.projectId }
+            ).then(data => {
+                commit("SAVE_COMMUNITIES", data);
+                return data;
+            });
+        },
+        GET_CITY_AREA({ commit, state }) {
+            if (Object.keys(state.city_area).length) {
+                return state.city_area;
+            }
+            return api('city_area');
+        }
+    }
 };
