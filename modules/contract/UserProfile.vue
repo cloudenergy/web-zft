258e307361016baaf0c88812f2627cb8c9fc72a9
ZSH-HSZ <31639046+ZSH-HSZ@users.noreply.github.com>
2018-03-16 09:28:55 +0800

    mobile Regular judgment

--- modules/contract/UserProfile.vue
+++ modules/contract/UserProfile.vue
@@ -20,14 +20,13 @@
 				</el-form-item>
 			</el-col>
 			<el-col :span="8">
-				<el-form-item prop="user.mobile" :rules="[
-      						{ required: true, message: '请输入手机号', trigger: 'blur' },
-							{ type:'number',min: 11, max: 11, message: '手机号输入错误', trigger: 'blur' }
-    					]">
-					<el-input class="form-input" placeholder="选填" v-model.number="user.mobile">
-						<template slot="prepend">电话</template>
-					</el-input>
-				</el-form-item>
+				<el-form :rules="rules">
+					<el-form-item prop="mobile">
+						<el-input class="form-input" placeholder="必填" v-model.number="user.mobile">
+							<template slot="prepend">电话</template>
+						</el-input>
+					</el-form-item>
+				</el-form>
 			</el-col>
 		</el-row>
 		<el-row>
@@ -65,6 +64,16 @@
 			}
 		},
 		data() {
+			var validateMobile = (rule, value, callback) => {
+				if (value === '') {
+					callback(new Error('请输入手机号'));
+				} else {
+					if (!(/^1[3|4|5|7|8][0-9]{9}$/.test(this.user.mobile))) {
+						callback(new Error('手机号输入错误'));
+					}
+					callback();
+				}
+			};
 			return {
 				idTypes: [{
 					id: 1,
@@ -90,7 +99,13 @@
 				}, {
 					id: 8,
 					name: '其他证件',
-				}]
+				}],
+				rules: {
+					mobile: [{
+						validator: validateMobile,
+						trigger: 'blur'
+					}]
+				}
 			}
 		}
 
