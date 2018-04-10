429349dab1cf3c2be2e638a12de984da7dc948e3
Louis Suo <louis.suo@myob.com>
2018-04-11 09:05:23 +1000

    fix port mapping of test server

--- docker/script/deploy.sh
+++ docker/script/deploy.sh
@@ -14,10 +14,10 @@ function start () {
 }
 function test_deploy() {
   docker rm -f test_deploy
-  docker run -d --name test_deploy -v ~/cert:/root/cert -p 1443:443 --net=zft $REPO:$IMAGE_VERSION
+  docker run -d --name test_deploy -v ~/cert:/root/cert -p 8080:80 -p 1443:443 --net=zft $REPO:$IMAGE_VERSION
 }
 function test_curl() {
-  response=$(curl --write-out %{http_code} --silent --output /dev/null https://localhost:1443)
+  response=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080)
   if [[ $response == "200" ]]; then
     docker rm -f test_deploy;
   else
