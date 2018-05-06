8b02025755f50c3f20f7004ee07a7117c85286f3
Louis Suo <louis.suo@myob.com>
2018-05-06 14:49:45 +1000

    fix api uri in zft-stage

--- plugins/api.js
+++ plugins/api.js
@@ -25,7 +25,7 @@ case 'development':
 	baseURL = '/api/v1.0';
 	break;
 case 'qa':
-	baseURL = 'https://zft-stage.cloudenergy.me/v1.0';
+	baseURL = 'https://zft-stage.cloudenergy.me/api/v1.0';
 	break;
 case 'production':
 	baseURL = 'https://api.51dianxiaoge.com/v1.0';
