9a7305afd27d49d47f9a875f212e557c2acc8507
Louis Suo <louis.suo@myob.com>
2018-06-27 02:45:37 +1000

    table column index changed

--- test/modules/devices/electricit.test.js
+++ test/modules/devices/electricit.test.js
@@ -16,13 +16,13 @@ describe('Electricit', () => {
   const wrapper = mount(Electricit, options);
   it('should have row-key', () => {
     expect(wrapper.html()).toContain(
-      '<td class="el-table_1_column_1  el-table__expand-column"><div class="cell"><div class="el-table__expand-icon "><i class="el-icon el-icon-arrow-right"></i></div></div></td>'
+      '<td class="el-table_1_column_2  el-table__expand-column"><div class="cell"><div class="el-table__expand-icon "><i class="el-icon el-icon-arrow-right"></i></div></div></td>'
     )
   });
 
   it('should hide deviceID prefix', () => {
     expect(wrapper.html()).toContain(
-      '<td class="el-table_1_column_3  "><div class="cell"><span>11223344</span> <br></div></td>'
+      '<td class="el-table_1_column_4  "><div class="cell"><span>11223344</span> <br></div></td>'
     )
   });
 
